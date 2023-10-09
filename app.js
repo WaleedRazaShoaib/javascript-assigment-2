481e8dfb8a5cf2b22562e6c10742a604a4c396ba
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2023-10-09 11:04:19 -0700

    assigment-2

---
+++ app.js
@@ -0,0 +1,214 @@
+//CHAPTER NO 9 T0 12;
+//    QUESTION NO 01; 
+
+// var user = prompt('enter yours city name');
+// if (user === 'karachi'){
+//     alert('Welcome to city of lights')
+// }
+
+//   QUESTION NO 02;
+// var user1 = prompt('Write your Gender');
+// if ( user1 === 'male' ){
+//     alert ("Welcome to my website sir")
+// }
+// else if (user1 == "female") {
+// alert("Welcome to my website ma'am") };
+//else {
+//alert("Error! please either write 'male' or 'female'")
+//}
+
+//  QUESTION NO 03;
+
+// var signal = prompt("Write the colour of signal");
+// if ( signal === "red"){
+//     alert("Must stop")
+// }
+// else if (signal === "yellow") { 
+// alert("Ready to move")
+// }
+// else { signal === "green"
+// alert("Move now")
+// };
+
+//   QUESTION NO 04;
+
+// var patrol = +prompt("how many fuel in your car");
+// if ( patrol < 0.25){
+//     alert("Please refill the fuel in your car ")
+// }
+
+//  QUESTION NO 05;
+
+//  var a = 4; if (++a === 5){ alert("given condition for variable a is true"); } 
+//  var b = 82; if (b++ === 83){ alert("given condition for variable b is true"); } 
+// //  var c = 12; if (c++ === 13){ alert("condition 1 is true"); } 
+// if (c === 13){ alert("condition 2 is true"); }
+//  if (++c < 14){ alert("condition 3 is true"); } 
+//  if(c === 14){ alert("condition 4 is true"); } 
+// //  var materialCost = 20000; var laborCost = 2000; 
+// var totalCost = materialCost + laborCost; 
+// if (totalCost === laborCost + materialCost){ alert("The cost equals"); } 
+// if (true){ alert("True"); } is smaller than cat"); } ;
+
+//QUESTION NO 06;
+
+// var maths = +prompt("Enter obtained marks of  maths :");
+// var english = +prompt("Enter obtained marks of english ");
+// var urdu = +prompt("Enter obtained marks of urdu ");
+// var totalMarks = +prompt("Enter the total marks for all subject :")
+// var obtainedmarks = maths + english + urdu;
+
+// var percent = obtainedmarks / totalMarks * 100;
+// if (percent >= 80 && percent <= 100) {
+//     alert( percent +"A+ Grade" + "\n" + "Excellent")
+// }
+//  else if (percent >= 70 && percent <= 80) {
+//     alert(percent +  " A Grade " + "\n" + "Good")
+// }
+//  else if (percent >= 60 && percent <= 50) {
+//     alert( percent + " B Grade " + "\n" + "Better")
+// }
+//  else if (percent >= 50 && percent <= 60) {
+//     alert( percent + " D Grade" + "\n" + "U need to be improve")
+// } 
+// else if (percent >= 0 && percent <= 50) {
+//     alert( percent + " F " + "\n" + "Sorry u have failed")
+// }
+// else { alert("U did not write correct marks ") 
+// };
+   
+//   QUESTION NO 07;
+
+// var secretnum = 7;
+// var user8 = +prompt("Guess the secret number");
+// if (secretnum === user8){
+//     alert("Bingo!")
+// }
+// else {
+//     alert ("Wrong answer")
+// // }
+
+//   QUESTION NO 08;
+
+// var user9  = +prompt("Enter your number to check if it's divisble by 3 or not!");
+// if (user9 % 3 === 0 ){
+//     alert("Yes, it's divisble by 3")
+// }
+// else {
+//     alert ("No, it's not divisble by 3")
+// }
+
+//   QUESTION NO 09  (not done );
+
+//   QUESTION NO 10;
+
+// var temp = +prompt("Write the temperature in your city");
+// if ( temp > 40 ){
+// alert ("Today! is very hot weather")
+// }
+// else if ( temp > 30 ){
+//     alert ("Today! the Weather is Normal")
+//     }
+// else if ( temp > 20 ){
+//     alert ("Today’s! Weather is cool ")
+//     }
+// else if ( temp > 10 ){
+//     alert ("OMG! Today’s weather is so Cool ")
+//     }
+//     else {
+//         alert("U did not write temperature")
+//     };
+
+//   QUESTION NO 11;
+
+// var value1 = +prompt("Enter your 1stValue ");
+// var sign  = prompt("Enter your operator ");
+// var value2 = +prompt("Enter your 2ndtValue ");
+//  if ( sign === '+' ){
+//     alert(value1 + value2)
+// }
+// else if ( sign === '-') {
+//     alert (value1 - value2)
+// }
+// else if ( sign === '*') {
+//     alert (value1 * value2)
+// }
+// else if ( sign === '/') {
+//     alert (value1 / value2)
+// }
+
+
+// CHAPTER NO 12 TO 13;    
+//   QUESTION NO 01;
+
+
+// var input = prompt("Enter a character (number or string):");
+// if (input.length === 1) {
+//     var charCode = input.charCodeAt(0); 
+    
+//     if (charCode >= 48 && charCode <= 57) {
+//         alert("The input is a number.");
+//     } else if (charCode >= 65 && charCode <= 90) {
+//         alert("The input is an uppercase letter.");
+//     } else if (charCode >= 97 && charCode <= 122) {
+//         alert("The input is a lowercase letter.");
+//     } else {
+//         alert("The input is a special character or symbol.");
+//     }
+// } else {
+//     alert("Please enter a single character (number or string).");
+// }
+
+//   QUESTIION NO 02;
+
+//var num1 = +prompt("Enter the first integer:");
+// var num2 = +prompt("Enter the second integer:");
+// var areEqual = num1 === num2;
+// var largerInteger;
+// if (areEqual) {
+//     largerInteger = "Both integers are equal.";
+// } else if (num1 > num2) {
+//     largerInteger = "The larger integer is: " + num1;
+// } else {
+//     largerInteger = "The larger integer is: " + num2;
+// }
+// alert(largerInteger);
+
+// QUESTION NO 03 (NOT DONE);
+
+//QUESTION NO 04 ;
+
+// var inputCharacter = prompt("Enter a character (string of length 1):");
+// if (inputCharacter.length === 1) {
+//     var result = isVowel(inputCharacter);
+//     if (result) {
+//         alert("The character '" + inputCharacter + "' is a vowel.");
+//     } else {
+//          alert("The character '" + inputCharacter + "' is not a vowel.");
+//      }
+//  } else {
+//      alert("Please enter a single character.");
+//  }
+
+//   QUESTION NO 05;
+
+// var passw = "walledraza123";
+// var askuser = prompt("Write your Password");
+// if (passw == askuser ){
+//     alert("U Write a Correct Password! Thanks")
+// }
+
+// else if (passw !== askuser){
+//     alert("U didn't Write The Correct Password")
+// }
+
+//  QUESTION NO 06;
+
+// var hour = 13; 
+// if (hour < 18) {
+//     alert("Good day") }
+//  else{ 
+//     alert("Good evening") }; 
+
+
+//  QUESTION NO 07 (NOT DONE); 
